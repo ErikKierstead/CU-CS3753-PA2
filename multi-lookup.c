cb987128e4b6b87e9e39604d54f505f7b079358d
user <user@cu-cs-vm.(none)>
2014-02-27 21:33:12 -0700

    Added multi-lookup.c

---
+++ multi-lookup.c
@@ -0,0 +1,162 @@
+/*
+ * File: lookup.c
+ * Author: Andy Sayler
+ * Project: CSCI 3753 Programming Assignment 2
+ * Create Date: 2012/02/01
+ * Modify Date: 2012/02/01
+ * Description:
+ * 	This file contains the reference non-threaded
+ *      solution to this assignment.
+ *  
+ */
+
+#include <stdlib.h>
+#include <stdio.h>
+#include <string.h>
+#include <errno.h>
+#include <pthread.h>
+#include <unistd.h>
+
+#include "util.h"
+
+#define MINARGS 3
+#define USAGE "<inputFilePath> <outputFilePath>"
+#define SBUFSIZE 1025
+#define INPUTFS "%1024s"
+#define NUM_THREADS 5
+
+void* ReadFile(void* fileName){
+    
+    printf("blah\n");
+
+    //Setup Local Vars and Handle void
+    //   long* tid = threadid;
+    long t;
+    long numprint = 3;
+    FILE* inputfp = (FILE*) fileName;
+    char hostname[SBUFSIZE];
+    char errorstr[SBUFSIZE];
+    char firstipstr[INET6_ADDRSTRLEN];
+ 
+    //Open Input File
+    inputfp = fopen(fileName, "r");
+
+    if(!inputfp){
+        sprintf(errorstr, "Error Opening Input File: %s", (char*)fileName);
+        perror(errorstr);
+        printf("Error Opening Input File: %s", (char*)fileName);
+        //break;
+    }
+
+    while(fscanf(inputfp, INPUTFS, hostname) > 0){
+
+    // Lookup hostname and get IP string 
+    if(dnslookup(hostname, firstipstr, sizeof(firstipstr))
+        == UTIL_FAILURE){
+        fprintf(stderr, "dnslookup error: %s\n", hostname);
+        strncpy(firstipstr, "", sizeof(firstipstr));
+        }
+
+        printf("%s: %s\n", hostname, firstipstr);
+
+        // Write to Output File
+        //fprintf(outputfp, "%s,%s\n", hostname, firstipstr);
+    }
+
+    //usleep(1000000000);
+
+    // Close Input File
+    fclose(inputfp);
+   
+     return NULL;
+}
+
+int main(int argc, char* argv[]){
+
+    /* Local Vars */
+    FILE* inputfp = NULL;
+    FILE* outputfp = NULL;
+    char hostname[SBUFSIZE];
+    char errorstr[SBUFSIZE];
+    char firstipstr[INET6_ADDRSTRLEN];
+    int i;
+    
+    /* Check Arguments */
+    if(argc < MINARGS){
+	fprintf(stderr, "Not enough arguments: %d\n", (argc - 1));
+	fprintf(stderr, "Usage:\n %s %s\n", argv[0], USAGE);
+	return EXIT_FAILURE;
+    }
+
+    //fprintf("hostname[SBUFSIZE] = %s", hostname);
+
+    //------------------------------added----------------
+    // Setup Local Vars
+    pthread_t threads[NUM_THREADS];
+    int rc;
+    long t;
+    //long cpyt[NUM_THREADS];
+
+    //long num_threads = NUM_THREADS;
+    long num_threads = argc - 1;
+    //long num_threads = 1;
+
+     // Spawn NUM_THREADS threads
+    for(t=0;t<num_threads;t++){
+	printf("In main: creating thread %ld\n", t);
+	
+        //cpyt[t] = t;
+	rc = pthread_create(&(threads[t]), NULL, ReadFile, (void*)argv[t+1]); //was &(cpyt[t]));
+	if (rc){
+	    printf("ERROR; return code from pthread_create() is %d\n", rc);
+	    exit(EXIT_FAILURE);
+	}
+    }
+
+
+//    return EXIT_SUCCESS;
+//}
+
+    //--------------------------------------------------
+
+    /* Open Output File */
+    outputfp = fopen(argv[(argc-1)], "w");
+    if(!outputfp){
+	perror("Error Opening Output File");
+	return EXIT_FAILURE;
+    }
+
+    // Loop Through Input Files
+    for(i=1; i<(argc-1); i++){
+	
+	// Open Input File 
+	inputfp = fopen(argv[i], "r");
+	if(!inputfp){
+	    //sprintf(errorstr, "Error Opening Input File: %s", argv[i]);
+	    perror(errorstr);
+	    break;
+	}	
+
+	// Read File and Process
+	while(fscanf(inputfp, INPUTFS, hostname) > 0){
+	
+	    // Lookup hostname and get IP string
+	    if(dnslookup(hostname, firstipstr, sizeof(firstipstr))
+	       == UTIL_FAILURE){
+		fprintf(stderr, "dnslookup error: %s\n", hostname);
+		strncpy(firstipstr, "", sizeof(firstipstr));
+	    }
+	
+	    // Write to Output File
+	    //fprintf(outputfp, "%s,%s\n", hostname, firstipstr);
+	}
+
+	//Close Input File
+	fclose(inputfp);
+    }
+
+    // Close Output File
+    //fclose(outputfp);
+
+    return EXIT_SUCCESS;
+}
